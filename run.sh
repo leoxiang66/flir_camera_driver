108ab141cb70191517719912525594acd7d428c0
Tao Xiang <niemalsaufgeben@yeah.net>
2024-09-25 10:09:58 +0800

    1

---
+++ run.sh
@@ -0,0 +1,8 @@
+ros2 launch spinnaker_camera_driver driver_node.launch.py camera_type:=flir_ax5 parameter_file:=/home/leo/git/flir_camera_driver/spinnaker_camera_driver/config/flir_ax5_rgb.yaml serial:="'18574774'"
+
+
+ros2 launch spinnaker_camera_driver driver_node.launch.py camera_type:=flir_ax5 parameter_file:=/home/leo/git/flir_camera_driver/spinnaker_camera_driver/config/blackfly_s.yaml serial:="'18574774'"
+
+
+ros2 launch spinnaker_camera_driver driver_node.launch.py camera_type:=flir_ax5 parameter_file:=/home/leo/git/flir_camera_driver/spinnaker_camera_driver/config/grasshopper.yaml serial:="'18574774'"
+
